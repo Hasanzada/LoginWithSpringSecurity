a6c5e27db2c9b56147b72ff564c6b5f5a2e75d5a
Ruslan <ruslanhesenzade0@gmail.com>
2018-08-02 19:08:35 +0400

    first commit

---
+++ LoginWithSpringSecurity/src/main/java/az/ruslan/LoginWithSpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package az.ruslan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithSpringSecurityApplication.class, args);
+	}
+}
---
+++ LoginWithSpringSecurity/src/test/java/az/ruslan/LoginWithSpringSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package az.ruslan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginWithSpringSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
